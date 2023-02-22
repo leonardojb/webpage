18fbb410d35dac26b17bf760a033b5a1b332fd88
leonardojb <111300203+leonardojb@users.noreply.github.com>
2023-02-21 22:05:05 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,3 +7,13 @@ profilePic.addEventListener('mouseout', function() {
   profilePic.style.transform = 'scale(1)';
   profilePic.style.transition = 'transform 0.5s';
 });
+
+// var img = document.getElementById("picjs");
+// img.addEventListener("mouseover", function() {
+//   img.classList.add("fade");
+//   img.src = "photo2.png";
+// });
+// img.addEventListener("mouseout", function() {
+//   img.classList.remove("fade");
+//   img.src = "photo.jpg";
+// });
