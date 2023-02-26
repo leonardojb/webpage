2ca53d17c04ea63f1cb30d386fbad91eae8f421b
leonardojb <111300203+leonardojb@users.noreply.github.com>
2023-02-26 20:52:04 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,6 +8,27 @@ profilePic.addEventListener('mouseout', function() {
   profilePic.style.transition = 'transform 0.5s';
 });
 
+const toggleSwitch = document.querySelector('#dark-mode-toggle');
+toggleSwitch.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('dark-mode', 'enabled');
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        localStorage.setItem('dark-mode', 'disabled');
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+});
+
+const currentTheme = localStorage.getItem('dark-mode');
+if (currentTheme === 'enabled') {
+    document.body.classList.add('dark-mode');
+    document.documentElement.setAttribute('data-theme', 'dark');
+} else {
+    document.body.classList.remove('dark-mode');
+    document.documentElement.setAttribute('data-theme', 'light');
+};
+
 // var img = document.getElementById("picjs");
 // img.addEventListener("mouseover", function() {
 //   img.classList.add("fade");
